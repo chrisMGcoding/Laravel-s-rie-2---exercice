ebf6995094b9ab1dc223e863985f3b9da500aafe
chris De Groef <chris.degroef@gmail.com>
2021-08-04 16:41:23 +0200

    Laravel : série 2 - exercice 1

---
+++ resources/views/taille.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+
+<ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('welcome')}}">Welcome</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('taille')}}">Taille</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('age')}}">Age</a>
+            </li>
+    </ul>
+    
+    <h3>taille : 176cm</h3>
+
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
